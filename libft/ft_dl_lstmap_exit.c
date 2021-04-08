27eafd1c81fd6b10f83b4d7d6228da1e77864199
ysoroko <ysoroko@student.s19.be>
2021-04-08 18:01:17 +0200

    commenting added on dl_lst fcts

--- libft/ft_dl_lstmap_exit.c
+++ libft/ft_dl_lstmap_exit.c
@@ -6,11 +6,18 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 17:47:28 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/04/08 17:56:16 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/04/08 18:00:24 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/libft.h"
+
+/*
+** FT_DL_LSTMAP_EXIT
+** Unlike its equivalent in t_list structures, any element given from t_dl_lst
+** will allow this function to apply f argument function to all of the dl_lst
+** list's elements, before and after the specified *l argument
+*/
 
 t_dl_lst	*ft_dl_lstmap_exit(t_dl_lst *l, void *(*f)(void *),
 															void (*del)(void *))
