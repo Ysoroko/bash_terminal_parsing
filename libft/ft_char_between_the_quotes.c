72cd95993e51f7ab932a42d0676f21c812e1206d
ysoroko <ysoroko@student.s19.be>
2021-04-10 17:55:25 +0200

    some comments added

--- libft/ft_char_between_the_quotes.c
+++ libft/ft_char_between_the_quotes.c
@@ -6,11 +6,18 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 15:46:30 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/04/10 17:52:33 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/04/10 17:55:07 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/libft.h"
+
+/*
+** FT_CHAR_BETWEEN_THE_QUOTES
+** This function will check if the character str_containing_c[char_position]
+** is included between quotes (single or double) and return
+** the corresponding quote or '\0' if the character is not between the quotes
+*/
 
 char	ft_char_between_the_quotes(int char_position, char *str_containing_c)
 {
