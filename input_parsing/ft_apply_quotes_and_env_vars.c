747802ddcca2f6b53bc4e5ef9863d4496b649d4c
ysoroko <ysoroko@student.s19.be>
2021-04-21 11:57:47 +0200

    two differnt functions added: "ft_single_quotes_copy" and "ft_doudvle_quotes_copy", but are still doing the same

--- input_parsing/ft_apply_quotes_and_env_vars.c
+++ input_parsing/ft_apply_quotes_and_env_vars.c
@@ -1,16 +1,50 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_apply_quotes.c                                  :+:      :+:    :+:   */
+/*   ft_apply_quotes_and_env_vars.c                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 16:43:56 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/04/16 12:03:46 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/04/21 11:47:41 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+/*
+** ft_single_quotes_copy
+** This function is responsible for applying single quotes to a string
+** Everything between two single quotes is copied as it is
+** Escape characters and environment variables are ignored
+*/
+
+static void	ft_single_quotes_copy(char **t_str, char **t_ret, int *k, int *l)
+{
+	while ((*t_str)[*k] && (*t_str)[*k] != '\'') 
+		{
+			(*t_ret)[*l] = (*t_str)[*k];
+			(*l)++;
+			(*k)++;
+		}
+}
+
+/*
+** ft_double_quotes_copy
+** This function is responsible for applying double quotes to a string
+** Everything between two single quotes is copied as it is
+** Escape characters and environment variables are ignored
+*/
+
+static void	ft_double_quotes_copy(char **t_str, char **t_ret, int *k, int *l)
+{
+	while ((*t_str)[*k] && (*t_str)[*k] != '\"') 
+		{
+			(*t_ret)[*l] = (*t_str)[*k];
+			(*l)++;
+			(*k)++;
+		}
+}
 
 static	void	ft_quoted_copy(char *str, char *ret, int *i, int *j)
 {
@@ -28,15 +62,10 @@ static	void	ft_quoted_copy(char *str, char *ret, int *i, int *j)
 	//printf("quote in quoted copy: [%c]\n i: [%d]\n j: [%d]\n", quote, *i, *j);
 	if (*i && str[*i - 1] == quote && ret[*j - 1] == quote)
 		l = -1;
-	if (quote == '\'' || quote == '\"')
-	{
-		while (temp_str[k] && temp_str[k] != quote) 
-		{
-			temp_ret[l] = temp_str[k];
-			l++;
-			k++;
-		}
-	}
+	if (quote == '\'')
+		ft_single_quotes_copy(&temp_str, &temp_ret, &k, &l);
+	else if (quote == '\"')
+		ft_double_quotes_copy(&temp_str, &temp_ret, &k, &l);
 	if (temp_str[k] == quote)
 		k++;
 	if (!k)
@@ -51,7 +80,7 @@ static	void	ft_quoted_copy(char *str, char *ret, int *i, int *j)
 }
 
 /*
-** FT_APPLY_QUOTES
+** FT_APPLY_QUOTES_AND_ENV_VARS
 ** This function will take a string filled in with quotes and if there
 ** are parts inside between quotes, it will "apply" those quotes.
 ** Returns a malloc'd string copy without quotes, but with quotes applied
@@ -59,7 +88,7 @@ static	void	ft_quoted_copy(char *str, char *ret, int *i, int *j)
 **			ft_apply_quotes('${PWD}') will return "${PWD}"
 */
 
-char	*ft_apply_quotes(char *str)
+char	*ft_apply_quotes_and_env_vars(char *str)
 {
 	int		i;
 	int		j;
