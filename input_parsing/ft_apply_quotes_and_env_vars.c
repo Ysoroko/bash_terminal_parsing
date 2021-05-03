32b55df15668bafe762f12eb67a61854bd0df2e8
ysoroko <ysoroko@student.s19.be>
2021-05-03 12:15:26 +0200

    work started on reworking ft_apply_quotes_and_env_vars

--- input_parsing/ft_apply_quotes_and_env_vars.c
+++ input_parsing/ft_apply_quotes_and_env_vars.c
@@ -6,13 +6,32 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 16:43:56 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/05/03 11:02:06 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/05/03 12:01:55 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-static int	ft_extract_env_variable()
+/*
+** ft_check_for_errors
+** This function simply checks if there are any wrong environmental
+** variable names present in str and if so, prints the related error
+** message using "strerror(errno)" and returns -1;
+** Returns the result from ft_calculate_total_length_needed otherwise
+** (a positive int)
+*/
+
+static int	ft_check_for_errors(char *str)
+{
+	int	ret;
+	
+	if (!str)
+		return (-1);
+	ret = ft_calculate_total_length_needed(str);
+	if (ret == -1)
+		strerror(errno);
+	return (ret);
+}
 
 /*
 ** FT_APPLY_QUOTES_AND_ENV_VARS
@@ -31,12 +50,12 @@ char	*ft_apply_quotes_and_env_vars(char **str)
 	char	*temp;
 	char	*ret;
 
-	if (!str || !(*str))
+	if (!str || !(*str) || ft_check_for_errors(*str) == -1)
 		return (0);
 	my_str = *str;
 	i = -1;
 	j = 0;
-	temp = ft_calloc_exit(ft_calculate_total_length_needed(*str));
+	temp = ft_calloc_exit(ft_calculate_total_length_needed(*str), 1);
 	while (my_str[++i])
 	{
 		if (my_str[i] == '\\')
